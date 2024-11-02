28118416dc47d9b0a4b194371d170e53491311ec
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2024-11-02 21:02:10 +0800

    before user registration
    before user registration

--- projects/views.py
+++ projects/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Project
 from django.http import HttpResponse
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
@@ -14,6 +16,7 @@ def project(request, pk):
     context = {'project': projectObj}
     return render(request, 'projects/single-project.html', context)
 
+@login_required(login_url='login')
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
@@ -26,7 +29,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
-
+@login_required(login_url='login')
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -40,6 +43,7 @@ def updateProject(request, pk):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url='login')
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
