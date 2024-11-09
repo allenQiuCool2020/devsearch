6074b1c176578aca35d98719453472acafecf460
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2024-11-09 16:00:21 +0800

    Before course 25
    Before course 25

--- users/views.py
+++ users/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 
 # Create your views here.
 def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
     if request.method == 'POST':
@@ -30,8 +32,27 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.warning(request,'User was logged out')
+    messages.info(request,'User was logged out')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('edit-account')
+        
+        else:
+            messages.error(request, 'An error has occurred during registration')
+    context = {'page':page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
@@ -45,3 +66,68 @@ def userProfile(request, pk):
     context = {'profile': profile, 'topSkills':topSkills, 'otherSkills':otherSkills}
     return render(request, 'users/user-profile.html', context)
 
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    context = {'profile': profile, 'skills':skills, 'projects': projects}
+    return render(request, 'users/account.html', context)
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid:
+            form.save()
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile = request.user.profile
+    print('profile:', profile)
+    form = SkillForm()
+    if request.method == 'POST':
+        form = SkillForm(request.POST)
+        if form.is_valid:
+            skill = form.save(commit=False)
+            skill.owner = profile
+            skill.save()
+            print('skill:', skill)
+            messages.success(request, 'Skill was added successfully')
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def updateSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    form = SkillForm(instance=skill)
+    if request.method == 'POST':
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Skill was updated')
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteSkill(request, pk):
+    profile = request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    if request.method == 'POST':
+        skill.delete()
+        messages.success(request, 'Skill was deleted successfully')
+        return redirect('account')
+    context = {'object': skill}
+    return render(request, 'delete-template.html', context)
